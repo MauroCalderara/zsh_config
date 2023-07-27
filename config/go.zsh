e5431f0b755eb1b2766cda21a4d63983b8f634c0
Mauro Calderara <MauroCalderara@users.noreply.github.com>
2023-07-26 22:46:08 -0400

    Fix PATH variable for go environment

--- config/go.zsh
+++ config/go.zsh
@@ -1,6 +1,6 @@
 # Go environment
 if command -v go >/dev/null && [[ -r "${HOME}/go/bin" ]]; then
-   GOPATH="${HOME}/go/bin"
-   PATH="${GOPATH}:${PATH}"
+   GOPATH="${HOME}/go"
+   PATH="${GOPATH}/bin:${PATH}"
    # Both Debian and Homebrew's go installation have GOROOT set correctly
 fi
